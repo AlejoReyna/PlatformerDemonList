fb5b1123abf96f0b76ed0901dadbdc68279242c7
eliasg24 <elias.guaj24@gmail.com>
2023-12-28 03:30:30 -0600

    Edit Demon Form Fixed

--- PlatformDemonList/urls.py
+++ PlatformDemonList/urls.py
@@ -4,7 +4,8 @@
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
+from django.http import HttpResponsePermanentRedirect
+from django.urls import path, re_path, include
 
 # Views
 from PlatformDemonList import views
