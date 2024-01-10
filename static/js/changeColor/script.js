c96210e513232e23200a898a3f499b455de694dc
Alberto Reyna <alexisreyna@MacBook-Pro-de-Alberto.local>
2024-01-10 04:50:00 -0600

    Updated Script

--- static/js/changeColor/script.js
+++ static/js/changeColor/script.js
@@ -4,13 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const demonCard = document.getElementsByClassName("demonCard");
     const news = document.getElementById("news");
     const nextUpdate = document.getElementById("nextUpdate");
-    const searchbar = document.getElementById("search");
     const projectHelpers = document.getElementById("projectHelpers");
     const listDevs = document.getElementById("listDevs");
     const TekoLight = document.getElementsByClassName("Teko-Light");
     const menuBtn = document.getElementById("menuBtn");
     const menuIcon = document.getElementById("menuIcon");
     const unrated = document.getElementById("unrated");
+    const cardText = document.getElementsByClassName("cardText")
     // Get searchbar
     const inputBar = document.getElementById("search");
     // Get their elements 
@@ -22,42 +22,51 @@ document.addEventListener('DOMContentLoaded', () => {
             navbar.style.backgroundColor = "#3e3e3e";
             // Demon card styles
             Array.from(demonCard).forEach(card => card.style.backgroundColor = "black");
-            Array.from(demonCard).forEach(card => card.style.background = 'linear-gradient(to bottom, #111, #333)');
+            Array.from(demonCard).forEach(card => card.style.background = 'linear-gradient(to right, #0a0f0d, #000000);');
             // Next update
             nextUpdate.style.backgroundColor = "black";
             nextUpdate.style.background = 'linear-gradient(to bottom, #111, #333)';
             news.style.backgroundColor = "black";
             // News
-            news.style.background = 'linear-gradient(to bottom, #111, #333)';
-            // Searchbar
-            searchbar.style.backgroundColor = "gray";
+            news.style.background = 'linear-gradient(to bottom, #2c2c2c, #000000);';
+            Array.from(cardText).forEach(element => element.style.color = "white");
             // Project Helpers
             projectHelpers.style.backgroundColor = "black";
             // ListDevs
             listDevs.style.backgroundColor = "black";
-            listDevs.style.background = 'linear-gradient(to bottom, #111, #333)';
+            listDevs.style.background = 'linear-gradient(to bottom, #2e2e2e 0%, #1c1c1c 50%, #000000 100%);';
             inputBar.style.backgroundColor = "#17181a";
+            inputBar.style.color = "white";
             Array.from(inputElements).forEach(element => element.style.color = "white");
             Array.from(TekoLight).forEach(element => element.style.color = "white");
             menuBtn.style.backgroundColor = "#17181a";
             menuIcon.style.backgroundColor = "white";
             // Unrated
             unrated.style.background = 'linear-gradient(to bottom, #111, #333)';
-
+            
 
 
         } else {
             navbar.style.backgroundColor = ""; // Resets to original color
             Array.from(demonCard).forEach(card => card.style.backgroundColor = ""); // Resets to original color
             nextUpdate.style.backgroundColor = "";
-            searchbar.style.backgroundColor = "";
+            nextUpdate.style.background = "";
+            news.style.backgroundColor = "";
+            // News
+            news.style.background = "";
+            Array.from(cardText).forEach(element => element.style.color = "");
+
             projectHelpers.style.backgroundColor = "";
             listDevs.style.backgroundColor = "";
+            listDevs.style.background = "";
             inputBar.style.backgroundColor = "";
             Array.from(inputElements).forEach(element => element.style.color = "");
             Array.from(TekoLight).forEach(element => element.style.color = "");
-            menuBtn.style.backgroundColor = "";
+            menuBtn.style.backgroundColor = "white";
             menuIcon.style.backgroundColor = "";
+            // Unrated 
+            unrated.style.background = "";
+
 
         }
         isOriginalColor = !isOriginalColor;
